207a9dd78dfd42a5b2794708317b36f55fd5febf
HoangDinh <dhoangk07@gmail.com>
2018-11-21 01:16:50 +0700

    Recorrect validate for Password model

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,5 +1,5 @@
 class Password < ApplicationRecord
   belongs_to :user
-  validates :pass, presence: true
-  validates :app, presence: true
+  validates :pass, :app,  presence: true
+  validates :app, uniqueness: true
 end
